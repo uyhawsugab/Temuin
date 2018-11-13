1fdebe0f0bf00a1ea5dbcf59bd73275750efa99e
rakandhiya <rakandhiya_daanii_26rpl@student.smktelkom-mlg.sch.id>
2018-11-13 13:19:57 +0700

    Initial commit

---
+++ pages/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/general.css">
+</head>
+<body>
+    <div class="root">
+        <nav class="navbar">
+            <a href="" class="nav-brand">Brand</a>
+            <div class="link-container">
+                <a href="#" class="nav-link">Sign In</a>
+                <a href="#" class="nav-link">Sign Up</a>
+            </div>
+        </nav>
+    </div>
+</body>
+</html>
